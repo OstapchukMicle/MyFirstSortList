6b69aa0a2c8d6eda7f9e921de7ee8acd2df2908f
OstapchukMicle <electron.micle@gmail.com>
2021-05-01 18:42:26 +0300

    init

---
+++ mySortList.py
@@ -0,0 +1,14 @@
+m = [7,3,0,2,-1]
+print(m)
+
+for j in range(0,len(m)):
+    temp = m[0]
+    for i in range(1,len(m)-j,1):
+        if temp>m[i]:
+            m[i-1]=m[i]
+            m[i]=temp
+        else:
+            temp=m[i]
+        print("подход:",j+1," проход:",i," список: ",m)
+
+    #print("подход:",j,"список: ",m)
