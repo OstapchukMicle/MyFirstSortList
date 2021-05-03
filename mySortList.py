baaa096bb8aab09c8fa967dc89602eb18f05bf53
OstapchukMicle <electron.micle@gmail.com>
2021-05-03 10:52:59 +0300

    добавил две функции sort_asc-которая сортирует массив по возврастанию и sort_desc-которая сортирует массив по убыванию

--- mySortList.py
+++ mySortList.py
@@ -1,14 +1,29 @@
-m = [7,3,0,2,-1]
-print(m)
+def sort_asc(m):
+    #m = [7,3,0,2,-1]
+    print(m)
 
-for j in range(0,len(m)):
-    temp = m[0]
-    for i in range(1,len(m)-j,1):
-        if temp>m[i]:
-            m[i-1]=m[i]
-            m[i]=temp
-        else:
-            temp=m[i]
-        print("подход:",j+1," проход:",i," список: ",m)
+    for j in range(0,len(m)):
+        temp = m[0]
+        for i in range(1,len(m)-j,1):
+            if temp>m[i]:
+                m[i-1]=m[i]
+                m[i]=temp
+            else:
+                temp=m[i]
+            #print("подход:",j+1," проход:",i," список: ",m)
+    return m
+        #print("подход:",j,"список:  ",m)
+def sort_desc(m):
+    #m = [7,3,0,2,-1]
+    print(m)
 
-    #print("подход:",j,"список:  ",m)
+    for j in range(0,len(m)):
+        temp = m[0]
+        for i in range(1,len(m)-j,1):
+            if temp<m[i]:
+                m[i-1]=m[i]
+                m[i]=temp
+            else:
+                temp=m[i]
+            #print("подход:",j+1," проход:",i," список: ",m)
+    return m
